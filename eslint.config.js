edc63d3988976eabf6df95ceeccbe3d87a2f746a
Ankit <vankit.kumar2294@gmail.com>
2026-06-28 19:19:08 +0530

    add workflows

--- eslint.config.js
+++ eslint.config.js
@@ -5,18 +5,47 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+// export default defineConfig([
+//   globalIgnores(['dist']),
+//   {
+//     files: ['**/*.{ts,tsx}'],
+//     extends: [
+//       js.configs.recommended,
+//       tseslint.configs.recommended,
+//       reactHooks.configs.flat.recommended,
+//       reactRefresh.configs.vite,
+//     ],
+//     languageOptions: {
+//       globals: globals.browser,
+//     },
+//   },
+// ])
+
+
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      // Other configs...
+
+      // Remove tseslint.configs.recommended and replace with this
+      tseslint.configs.recommendedTypeChecked,
+      // Alternatively, use this for stricter rules
+      tseslint.configs.strictTypeChecked,
+      // Optionally, add this for stylistic rules
+      tseslint.configs.stylisticTypeChecked,
+
+      // Other configs...
     ],
     languageOptions: {
-      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+        globals: globals.browser,
+      },
+      // other options...
     },
   },
 ])
+
